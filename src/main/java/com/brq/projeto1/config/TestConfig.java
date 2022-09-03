2bf3acc0d403a94c320cd12acf15ad40d9f8baf8
Wiliam GM <gomesruivo@gmail.com>
2022-09-03 13:20:19 -0300

    Criada a Classe OrderItem associando Product e Order

--- src/main/java/com/brq/projeto1/config/TestConfig.java
+++ src/main/java/com/brq/projeto1/config/TestConfig.java
@@ -1,15 +1,9 @@
 package com.brq.projeto1.config;
 
 
-import com.brq.projeto1.entities.Category;
-import com.brq.projeto1.entities.Order;
-import com.brq.projeto1.entities.Product;
-import com.brq.projeto1.entities.User;
+import com.brq.projeto1.entities.*;
 import com.brq.projeto1.entities.enums.OrderStatus;
-import com.brq.projeto1.repositories.CategoryRepository;
-import com.brq.projeto1.repositories.OrderRepository;
-import com.brq.projeto1.repositories.ProductRepository;
-import com.brq.projeto1.repositories.UserRepository;
+import com.brq.projeto1.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -33,6 +27,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -69,5 +66,13 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
     }
 }
